e83336bc1b0f9b677a763b1e8feac2d94d614a97
Tanmayrajput08 <tanmayrajput1778@gmail.com>
2025-05-14 11:37:01 +0530

    added some documents

--- tender_management/tender_management/urls.py
+++ tender_management/tender_management/urls.py
@@ -14,9 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
+from django.conf import settings
+
+admin.site.site_header = "PCMC Tender Management"
+admin.site.site_title = "PCMC Admin Portal"
+admin.site.index_title = "Welcome to PCMC Tender Dashboard"
+
+
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
